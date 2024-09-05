1d3bea59989c1482275dc7ab7391c49ceff21efd
raxor <jannik.markau01@gmail.com>
2024-09-05 18:01:04 +0200

    Hochladen des Projekts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,66 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, Renderer2} from '@angular/core';
+import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
+import {filter} from "rxjs";
+import {collection, Firestore, getDocs} from "@angular/fire/firestore";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
+
 export class AppComponent {
-  title = 'sicher-im-netz';
+  title = 'Sicher-Im-Netz';
+  testData: any;
+  firestore = inject(Firestore);
+
+  constructor(private renderer: Renderer2, private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      const currentRoute = this.router.routerState.root.snapshot.firstChild;
+      if (currentRoute && currentRoute.data['background']) {
+        const colors = currentRoute.data['background'];
+        document.body.style.background = `linear-gradient(to bottom right, ${colors[0]}, ${colors[1]})`;
+      } else {
+        document.body.style.background = 'linear-gradient(to bottom right, #e0f7fa, rgba(0, 161, 142, 0.56))'; // Standardfarbe
+      }
+    });
+
+    getDocs(collection(this.firestore, "testPath")).then((response) => {
+      console.log(response.docs)
+    });
+
+    this.createBinaryAnimation();
+  }
+
+  createBinaryAnimation() {
+    const animationContainer = document.querySelector('.background-animation');
+    setInterval(() => {
+      const binaryElement = this.renderer.createElement('div');
+      this.renderer.addClass(binaryElement, 'binary-animation');
+      this.renderer.setProperty(binaryElement, 'innerText', this.generateRandomBinaryString());
+      this.renderer.setStyle(binaryElement, 'left', `${Math.random() * 100}%`);
+      this.renderer.setStyle(binaryElement, 'animationDuration', `${5 + Math.random() * 5}s`);
+      this.renderer.appendChild(animationContainer, binaryElement);
+
+      // Entferne das Element nach der Animation
+      setTimeout(() => {
+        this.renderer.removeChild(animationContainer, binaryElement);
+      }, 10000);
+    }, 1000);
+  }
+
+  generateRandomBinaryString(): string {
+    const length = Math.floor(Math.random() * 5) + 3; // Zufällige Länge zwischen 3 und 7
+    let binaryString = '';
+    for (let i = 0; i < length; i++) {
+      binaryString += Math.random() < 0.5 ? '0' : '1';
+    }
+    return binaryString;
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { IntroductionComponent} from "./course/introduction/introduction.component";
+import { HomeComponent} from "./home/home.component";
+import { PasswordsComponent} from "./course/passwords/passwords.component";
+import { SocialNetworkComponent} from "./course/social-network/social-network.component";
+import { CommunicationComponent} from "./course/communication/communication.component";
+import {CoursePasswordComponent} from "./course-password/course-password.component";
+import {RegisterComponent} from "./register/register.component";
+import {LoginComponent} from "./login/login.component";
+import {SettingsComponent} from "./settings/settings.component";
+import {SmartphoneComponent} from "./course/smartphone/smartphone.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'password/:course', component: CoursePasswordComponent },
+  { path: 'kurs/introduction', component: IntroductionComponent, data: { background: ['rgba(130,207,134,0.78)', 'rgb(196,221,183)'] }},
+  { path: 'kurs/passwordSecurity', component: PasswordsComponent, data: { background: ['rgb(253,238,154)', 'rgb(247,241,194)']}},
+  { path: 'kurs/socialNetwork', component: SocialNetworkComponent },
+  { path: 'kurs/communication', component: CommunicationComponent },
+  { path: 'kurs/smartphone', component: SmartphoneComponent, data: { background: ['rgb(253,238,154)', 'rgb(247,241,194)']}},
+  { path: 'kurs/register', component: RegisterComponent },
+  { path: 'kurs/login', component: LoginComponent },
+  { path: 'settings', component: SettingsComponent },
+];
